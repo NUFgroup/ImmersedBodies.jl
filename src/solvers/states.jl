676bb1760a6c71feab4cdbf82e48646491e0585e
Liam Nelson <liamnelson0505@gmail.com>
2023-06-08 10:07:18 -0500

    Preliminary implementation of the streamfunction for a moving grid.
    TODO: Pass unit tests, then create better ones.

--- src/solvers/states.jl
+++ src/solvers/states.jl
@@ -1,3 +1,5 @@
+using Debugger
+
 """
     PsiOmegaState
 
@@ -140,7 +142,7 @@ function Quantities.streamfunction(
 
     return grid_quantity(coords) do state::StatePsiOmegaGridCNAB
         qty = quantities(state)
-
+        @bp
         u, v = qty.u
         ψ = unflatten_circ(qty.ψ, prob.fluid.gridindex, level)
         for i in axes(ψ, 3)
@@ -152,11 +154,96 @@ function Quantities.streamfunction(
     end
 end
 
+function temp_f(prob::Problem{PsiOmegaFluidGrid{CNAB,OffsetFrame{GlobalFrame}}}; level=all_levels(prob))
+    # TODO: Implement for moving domain
+    # return error("streamfunction not implemented for moving domains")
+
+    # Implementation for non-rotating motion:
+    fluid = prob.fluid
+    frame = fluid.frame
+    # grids = discretized(prob.fluid)
+
+    # global_fluid = PsiOmegaFluidGrid(conditions(fluid), baselevel(grids); scheme=timestep_scheme(fluid))
+    # absolute_prob = Problem(global_fluid, prob.bodies)
+    # absolute_ψ = Quantities.streamfunction(absolute_prob)
+
+    # relative_ψ = grid_quantity(coords) do state::StatePsiOmegaGridCNAB
+    #     for i in axes(ψ, 3)
+    #         xs, ys = coords[i]
+    #         @views ψ[:, :, i] .+= 
+
+    # return 
+    
+    # return absolute_ψ .+ relative_ψ
+
+    grids = discretized(fluid)
+    coords = [circ_ranges(sublevel(grids, lev)) for lev in level]
+
+    return grid_quantity(coords) do state::StatePsiOmegaGridCNAB
+        qty = quantities(state)
+
+        u, v = qty.u
+        instant = frame.f(state.t)
+        @bp
+        v0, u0 = instant.v
+        ψ = unflatten_circ(qty.ψ, fluid.gridindex, level)
+
+        for i in axes(ψ, 3)
+            xs, ys = coords[i]
+            @views ψ[:, :, i] .+= ((u - u0) * y - (v - v0) * x for (x, y) in Iterators.product(xs, ys))
+        end
+        @bp
+        return ψ
+    end
+end
+
 function Quantities.streamfunction(
     prob::Problem{PsiOmegaFluidGrid{CNAB,OffsetFrame{GlobalFrame}}}; level=all_levels(prob)
 )
     # TODO: Implement for moving domain
-    return error("streamfunction not implemented for moving domains")
+    # return error("streamfunction not implemented for moving domains")
+
+    # Implementation for non-rotating motion:
+    fluid = prob.fluid
+    frame = fluid.frame
+    # grids = discretized(prob.fluid)
+
+    # global_fluid = PsiOmegaFluidGrid(conditions(fluid), baselevel(grids); scheme=timestep_scheme(fluid))
+    # absolute_prob = Problem(global_fluid, prob.bodies)
+    # absolute_ψ = Quantities.streamfunction(absolute_prob)
+
+    # relative_ψ = grid_quantity(coords) do state::StatePsiOmegaGridCNAB
+    #     for i in axes(ψ, 3)
+    #         xs, ys = coords[i]
+    #         @views ψ[:, :, i] .+= 
+
+    # return 
+    
+    # return absolute_ψ .+ relative_ψ
+
+    grids = discretized(fluid)
+    coords = [circ_ranges(sublevel(grids, lev)) for lev in level]
+    @bp
+
+    return grid_quantity(coords) do state::StatePsiOmegaGridCNAB
+        qty = quantities(state)
+        u, v = qty.u
+        instant = frame.f(state.t)
+        @bp
+        v0, u0 = instant.v
+        ψ = unflatten_circ(qty.ψ, fluid.gridindex, level)
+
+        for i in axes(ψ, 3)
+            xs, ys = coords[i]
+            @views ψ[:, :, i] .+= ((u - u0) * y - (v - v0) * x for (x, y) in Iterators.product(xs, ys))
+        end
+        @bp
+        return ψ
+    end
+
+    
+    ψ = temp_f(prob; level)
+    return ψ
 end
 
 """
--- test/quantities.jl
+++ test/quantities.jl
@@ -1,27 +1,77 @@
 module TestQuantities
 
 using ImmersedBodies.Quantities
+using ImmersedBodies
+using Debugger
 using Test
-using Plots
+# using Plots
 
 @testset "quantities" begin
-    @testset "plotting" begin
-        let xs = LinRange(0, 1, 3), ys = LinRange(0, 2, 4)
-            z = xs .+ ys'
-            val = GridValue(z, (xs, ys))
 
-            p, = plot(val)
-            @test p[1][:seriestype] == :heatmap # Make sure the recipe was applied
+    # @testset "plotting" begin
+    #     let xs = LinRange(0, 1, 3), ys = LinRange(0, 2, 4)
+    #         z = xs .+ ys'
+    #         val = GridValue(z, (xs, ys))
+
+    #         p, = plot(val)
+    #         @test p[1][:seriestype] == :heatmap # Make sure the recipe was applied
+    #     end
+
+    #     let coords = [(LinRange(-t, t, 6), LinRange(-0.5t, 0.5t, 3)) for t in 2.0 .^ (0:2)]
+    #         z = cat((xs .+ ys' for (xs, ys) in coords)...; dims=3)
+    #         val = MultiLevelGridValue(z, coords)
+
+    #         p, = plot(val)
+    #         @test p[1][:seriestype] == :heatmap # Make sure the recipe was applied
+    #         @test length(p.series_list) == size(z, 3) # One heatmap per sublevel
+    #     end
+    # end
+
+    @testset "moving grid quantities" begin
+        dx = 0.02
+        xspan = (-1.0, 3.0)
+        yspan = (-2.0, 2.0)
+        basegrid = UniformGrid(dx, xspan, yspan)
+        grids = MultiLevelGrid(basegrid, 5)
+
+        scheme = default_scheme(basegrid; Umax=hypot(1, 2), cfl=0.2, safety=2.0)
+        dt = timestep(scheme)
+
+        flow_vec = (1.0, 2.0)
+
+        Re = 100.0
+        flow = FreestreamFlow(t -> flow_vec; Re=Re)
+        flow_0 = FreestreamFlow(t -> (0.0, 0.0), Re=Re)
+
+        frame = OffsetFrame(GlobalFrame()) do t
+            r = (0.0, 0.0)
+            v = .- flow_vec
+            θ = 0.0
+            Ω = 0.0
+            return OffsetFrameInstant(r, v, θ, Ω)
         end
+        
+        flowingfluid = PsiOmegaFluidGrid(flow, grids; scheme)
+        nonflowingfluid = PsiOmegaFluidGrid(flow_0, grids; scheme, frame)
 
-        let coords = [(LinRange(-t, t, 6), LinRange(-0.5t, 0.5t, 3)) for t in 2.0 .^ (0:2)]
-            z = cat((xs .+ ys' for (xs, ys) in coords)...; dims=3)
-            val = MultiLevelGridValue(z, coords)
+        curve = Curves.Circle(0.5)
+        stationarybody = RigidBody(partition(curve, flowingfluid))
+        stationarybodies = BodyGroup([stationarybody])
+        movingbody = RigidBody(partition(curve, nonflowingfluid))
+        movingbodies = BodyGroup([movingbody])
 
-            p, = plot(val)
-            @test p[1][:seriestype] == :heatmap # Make sure the recipe was applied
-            @test length(p.series_list) == size(z, 3) # One heatmap per sublevel
-        end
+        flowprob = Problem(flowingfluid, stationarybodies)
+        moveprob = Problem(nonflowingfluid, movingbodies)
+
+        flowstate = initstate(flowprob)
+        movestate = initstate(moveprob)
+
+        solve!(flowstate, flowprob, 10 * dt)
+        solve!(movestate, moveprob, 10 * dt)
+
+        # @run @test Quantities.streamfunction(flowprob)(flowstate) ≈ Quantities.streamfunction(moveprob)(movestate)
+        @run Quantities.streamfunction(flowprob)(flowstate)
+        @run Quantities.streamfunction(moveprob)(movestate)
     end
 end
 
